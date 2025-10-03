0ee7b023b1fc65e73d03c09fa0d9cf2c46896d50
s27523 <s27523@pjwstk.edu.pl>
2025-10-03 13:06:36 +0200

    Initial commit

---
+++ src/main/java/edu/pjwstk/gami_life/GamiLifeApplication.java
@@ -0,0 +1,13 @@
+package edu.pjwstk.gami_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamiLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamiLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pjwstk/gami_life/GamiLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pjwstk.gami_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamiLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
