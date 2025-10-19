59cee7da17f43860798feee19c686b1beb6c31b4
s27523 <s27523@pjwstk.edu.pl>
2025-10-19 15:56:46 +0200

    refactor: naming change
    Change naming associated with create invitation use case.

--- groups/src/main/java/edu/pjwstk/groups/usecase/creategroupinvitation/CreateGroupInvitationMapper.java
+++ groups/src/main/java/edu/pjwstk/groups/usecase/creategroupinvitation/CreateGroupInvitationMapper.java
@@ -3,13 +3,11 @@ package edu.pjwstk.groups.usecase.creategroupinvitation;
 import edu.pjwstk.groups.entity.Group;
 import edu.pjwstk.groups.entity.GroupInvitation;
 import edu.pjwstk.groups.entity.InvitationStatus;
-import edu.pjwstk.groups.usecase.creategroupmember.CreateGroupMemberResponse;
-import edu.pjwstk.groups.usecase.editgroupinvitationstatus.EditGroupInvitationStatusResponse;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface CreateGroupInvitationStatusMapper {
+public interface CreateGroupInvitationMapper {
 
     GroupInvitation toEntity(Group groupInvited, InvitationStatus invitationStatus, UUID userId,
                              LocalDateTime expiresAt, String link, UUID groupInvitationId);
