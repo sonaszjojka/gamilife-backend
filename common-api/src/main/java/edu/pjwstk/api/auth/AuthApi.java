c2b7ffc2ad93e678c3a25c89b5c59638e4ff7748
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-11-04 01:39:44 +0100

    feat: remove deprecated from getCurrentUser

--- common-api/src/main/java/edu/pjwstk/api/auth/AuthApi.java
+++ common-api/src/main/java/edu/pjwstk/api/auth/AuthApi.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 public interface AuthApi {
 
-    @Deprecated(forRemoval = true)
     Optional<CurrentUserDto> getCurrentUser();
 
     String handleChangePassword(ChangePasswordCommand dto);
