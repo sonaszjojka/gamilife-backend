e65f07539329f500bcfc7d1b454fd6aa334a2399
s27523 <s27523@pjwstk.edu.pl>
2025-11-18 21:08:55 +0100

    feat: get all group requests

--- groups/src/main/java/edu/pjwstk/groups/controller/GroupRequestController.java
+++ groups/src/main/java/edu/pjwstk/groups/controller/GroupRequestController.java
@@ -10,7 +10,12 @@ import edu.pjwstk.groups.usecase.deletegrouprequest.DeleteGroupRequestUseCase;
 import edu.pjwstk.groups.usecase.editgrouprequeststatusforgrouprequest.EditGroupRequestStatusForGroupRequestCommand;
 import edu.pjwstk.groups.usecase.editgrouprequeststatusforgrouprequest.EditGroupRequestStatusForGroupRequestResult;
 import edu.pjwstk.groups.usecase.editgrouprequeststatusforgrouprequest.EditGroupRequestStatusForGroupRequestUseCase;
+import edu.pjwstk.groups.usecase.getgrouprequests.GetGroupRequestsCommand;
+import edu.pjwstk.groups.usecase.getgrouprequests.GetGroupRequestsResult;
+import edu.pjwstk.groups.usecase.getgrouprequests.GetGroupRequestsUseCase;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +31,8 @@ public class GroupRequestController {
     private final CreateGroupRequestUseCase createGroupRequestUseCase;
     private final DeleteGroupRequestUseCase deleteGroupRequestUseCase;
     private final EditGroupRequestStatusForGroupRequestUseCase editGroupRequestStatusForGroupRequestUseCase;
+    private final GetGroupRequestsUseCase getGroupRequestsUseCase;
+
 
     @PostMapping
     private ResponseEntity<CreateGroupRequestResult> save(@PathVariable("groupId") UUID groupId) {
@@ -58,6 +65,24 @@ public class GroupRequestController {
                         groupId,
                         groupRequestId,
                         request.groupRequestStatusId()
+                )
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<GetGroupRequestsResult> getAllGroupRequests(
+            @PathVariable("groupId") UUID groupId,
+            @RequestParam(required = false) Integer statusId,
+            @RequestParam(defaultValue = "0") @Min(0) Integer page,
+            @RequestParam(defaultValue = "20") @Min(1) @Max(100) Integer size) {
+
+        GetGroupRequestsResult response = getGroupRequestsUseCase.execute(
+                new GetGroupRequestsCommand(
+                        groupId,
+                        statusId,
+                        page,
+                        size
                 )
         );
         return ResponseEntity.ok(response);
