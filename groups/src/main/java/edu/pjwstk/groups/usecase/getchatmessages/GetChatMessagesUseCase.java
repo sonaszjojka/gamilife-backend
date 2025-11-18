71f51a98ce12c3debdb226beda02c2fc0f1a71a3
s27523 <s27523@pjwstk.edu.pl>
2025-11-18 20:52:22 +0100

    feat: get all messages

---
+++ groups/src/main/java/edu/pjwstk/groups/usecase/getchatmessages/GetChatMessagesUseCase.java
@@ -0,0 +1,6 @@
+package edu.pjwstk.groups.usecase.getchatmessages;
+
+import edu.pjwstk.core.UseCase;
+
+public interface GetChatMessagesUseCase extends UseCase<GetChatMessagesCommand, GetChatMessagesResult> {
+}
