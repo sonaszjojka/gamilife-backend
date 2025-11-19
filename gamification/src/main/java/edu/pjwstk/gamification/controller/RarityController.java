ae0b6e8143b13ba661e508288abe16eb42367da9
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-11-19 18:40:31 +0100

    feat: add get for all item rarities

--- gamification/src/main/java/edu/pjwstk/gamification/controller/RarityController.java
+++ gamification/src/main/java/edu/pjwstk/gamification/controller/RarityController.java
@@ -1,16 +1,27 @@
 package edu.pjwstk.gamification.controller;
 
+import edu.pjwstk.gamification.usecase.getallitemrarities.GetAllItemRaritiesCommand;
+import edu.pjwstk.gamification.usecase.getallitemrarities.GetAllItemRaritiesResult;
+import edu.pjwstk.gamification.usecase.getallitemrarities.GetAllItemRaritiesUseCase;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/item-rarities")
 public class RarityController {
 
+    private final GetAllItemRaritiesUseCase getAllItemRaritiesUseCase;
+
     @GetMapping
-    public ResponseEntity<String> getRarities() {
-        return ResponseEntity.ok("Hello World, Rarity Controller!");
+    public ResponseEntity<GetAllItemRaritiesResult> getRarities() {
+        return ResponseEntity.ok(
+                getAllItemRaritiesUseCase.execute(
+                        new GetAllItemRaritiesCommand()
+                )
+        );
     }
 }
