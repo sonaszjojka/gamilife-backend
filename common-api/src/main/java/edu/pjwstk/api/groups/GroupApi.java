63591d7f6871ac6b30ced7ab738b28252fe195ff
s27523 <s27523@pjwstk.edu.pl>
2025-11-28 20:26:36 +0100

    fix: fix loading collection (group members) v2

--- common-api/src/main/java/edu/pjwstk/api/groups/GroupApi.java
+++ common-api/src/main/java/edu/pjwstk/api/groups/GroupApi.java
@@ -4,12 +4,19 @@ package edu.pjwstk.api.groups;
 import edu.pjwstk.api.groups.dto.FindAllGroupsByUserIdWhereUserIsMemberResult;
 import edu.pjwstk.api.groups.dto.GroupDto;
 import edu.pjwstk.api.groups.dto.GroupMemberDto;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 public interface GroupApi {
+
+    @Transactional(readOnly = true)
     GroupMemberDto findGroupMemberById(UUID groupMemberId);
+
+    @Transactional(readOnly = true)
     GroupDto findGroupById(UUID groupId);
+
+    @Transactional(readOnly = true)
     FindAllGroupsByUserIdWhereUserIsMemberResult findAllGroupsByUserIdWhereUserIsMember(UUID userId,
                                                                                         Integer page,
                                                                                         Integer size,
