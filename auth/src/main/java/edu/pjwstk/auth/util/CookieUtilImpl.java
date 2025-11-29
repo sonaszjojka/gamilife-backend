5146627b6a91601ebc4d5f6f18bd06425228fae8
s27523 <s27523@pjwstk.edu.pl>
2025-11-29 22:52:33 +0100

    refactor: add comment for dev config to cookies

--- auth/src/main/java/edu/pjwstk/auth/util/CookieUtilImpl.java
+++ auth/src/main/java/edu/pjwstk/auth/util/CookieUtilImpl.java
@@ -45,7 +45,7 @@ public class CookieUtilImpl implements CookieUtil {
                 .httpOnly(true)
                 .maxAge(Duration.ofSeconds(expiry))
                 .secure(secure)
-                .sameSite("None")
+                .sameSite("None") // comment on dev profile
                 .build();
     }
 }
