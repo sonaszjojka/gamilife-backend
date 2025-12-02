914aefa87f29b8b73449eaeb047ebc659abb36e1
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-12-02 22:44:43 +0100

    refactor: move business modules to separate module

---
+++ modules/auth/src/main/java/edu/pjwstk/auth/security/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package edu.pjwstk.auth.security;
+
+import edu.pjwstk.auth.service.TokenService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = extractToken(request);
+        if (token != null) {
+            try {
+                Claims claims = tokenService.validateTokenAndExtractClaims(token);
+                String username = claims.getSubject();
+                UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
+                Instant issuedAt = claims.getIssuedAt().toInstant();
+
+                if (userDetails.getPasswordChangeDate().isAfter(issuedAt)) {
+                    throw new BadCredentialsException("Bad credentials");
+                }
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (UsernameNotFoundException | BadCredentialsException e) {
+                SecurityContextHolder.getContext().setAuthentication(null);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("ACCESS-TOKEN".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ modules/user/src/main/java/edu/pjwstk/user/persistence/UserEntity.java
@@ -0,0 +1,69 @@
+package edu.pjwstk.user.persistence;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+@DynamicUpdate // worth considering
+public class UserEntity {
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    @Column(name = "level", nullable = false)
+    private int level;
+
+    @Column(name = "experience", nullable = false)
+    private int experience;
+
+    @Column(name = "money", nullable = false)
+    private int money;
+
+    @Column(name = "send_budget_reports", nullable = false)
+    private boolean sendBudgetReports;
+
+    @Column(name = "is_profile_public", nullable = false)
+    private boolean isProfilePublic;
+
+    @Column(name = "is_email_verified", nullable = false)
+    private boolean isEmailVerified;
+
+    @Column(name = "is_tutorial_completed", nullable = false)
+    private boolean isTutorialCompleted;
+
+    @Column(name = "password_change_date", nullable = false)
+    private Long passwordChangeDate;
+
+}
