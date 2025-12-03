3431227351e94ef45efd1fc3470e3c210c79db80
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-12-03 02:35:48 +0100

    chore: unused code clean-up

--- modules/task/src/main/java/pl/gamilife/task/TasksApiImpl.java
+++ modules/task/src/main/java/pl/gamilife/task/TasksApiImpl.java
@@ -1,5 +1,6 @@
 package pl.gamilife.task;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.gamilife.api.task.TasksApi;
 import pl.gamilife.api.task.dto.TaskDto;
@@ -8,24 +9,16 @@ import pl.gamilife.api.task.dto.TaskForGroupTaskResponseDto;
 import pl.gamilife.task.application.createtaskforgrouptask.CreateTaskForGroupTaskUseCase;
 import pl.gamilife.task.application.deletetask.DeleteTaskUseCase;
 import pl.gamilife.task.application.findtaskbyid.FindTaskByIdUseCase;
-import pl.gamilife.task.application.taskexistsbyid.ExistsByTaskIdUseCase;
 
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class TasksApiImpl implements TasksApi {
 
-    private final ExistsByTaskIdUseCase existsByTaskIdUseCase;
     private final FindTaskByIdUseCase findTaskByIdUseCase;
     private final DeleteTaskUseCase deleteTaskUseCase;
     private final CreateTaskForGroupTaskUseCase createTaskForGroupTaskUseCase;
-
-    public TasksApiImpl(ExistsByTaskIdUseCase existsByTaskIdUseCase, FindTaskByIdUseCase findTaskByIdUseCase, DeleteTaskUseCase deleteTaskUseCase, CreateTaskForGroupTaskUseCase createTaskForGroupTaskUseCase) {
-        this.existsByTaskIdUseCase = existsByTaskIdUseCase;
-        this.findTaskByIdUseCase = findTaskByIdUseCase;
-        this.deleteTaskUseCase = deleteTaskUseCase;
-        this.createTaskForGroupTaskUseCase = createTaskForGroupTaskUseCase;
-    }
 
     @Override
     public TaskDto findTaskByTaskId(UUID taskId) {
