7ea0f8404e8bd2eda47df11d8efa2c83cf9eda06
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-08 15:00:04 +0100

    feat: add get for all store items endpoint

--- modules/gamification/src/main/java/pl/gamilife/gamification/infrastructure/web/StoreController.java
+++ modules/gamification/src/main/java/pl/gamilife/gamification/infrastructure/web/StoreController.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.gamilife.gamification.application.usecase.getstoreitems.getall.GetStoreItemsCommand;
+import pl.gamilife.gamification.application.usecase.getstoreitems.getall.GetStoreItemsResult;
+import pl.gamilife.gamification.application.usecase.getstoreitems.getall.GetStoreItemsUseCase;
 
 import java.util.UUID;
 
@@ -11,15 +14,28 @@ import java.util.UUID;
 @RequestMapping("/api/v1/store")
 public class StoreController {
 
+    private final GetStoreItemsUseCase getStoreItemsUseCase;
+
+    public StoreController(GetStoreItemsUseCase getStoreItemsUseCase) {
+        this.getStoreItemsUseCase = getStoreItemsUseCase;
+    }
+
     @GetMapping("/items")
-    public ResponseEntity<String> getFilteredItems(
+    public ResponseEntity<GetStoreItemsResult> getFilteredItems(
             @RequestParam(required = false) String itemName,
             @RequestParam(required = false) Integer itemSlot,
             @RequestParam(required = false) Integer rarity,
             @RequestParam(defaultValue = "0") @Min(0) Integer page,
             @RequestParam(defaultValue = "10") @Min(1) @Max(100) Integer size
     ) {
-        return ResponseEntity.ok("Hello World, Store Controller!");
+        GetStoreItemsCommand cmd = new GetStoreItemsCommand(
+                itemName,
+                itemSlot,
+                rarity,
+                page,
+                size
+        );
+        return ResponseEntity.ok(getStoreItemsUseCase.execute(cmd));
     }
 
     @GetMapping("/items/{itemId}")
