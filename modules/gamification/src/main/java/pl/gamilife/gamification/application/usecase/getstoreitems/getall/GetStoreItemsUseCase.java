335a7040dc596d38a1874adc865754dafe7d8e42
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-08 15:22:36 +0100

    refactor: change response format to page of store item dtos

--- modules/gamification/src/main/java/pl/gamilife/gamification/application/usecase/getstoreitems/getall/GetStoreItemsUseCase.java
+++ modules/gamification/src/main/java/pl/gamilife/gamification/application/usecase/getstoreitems/getall/GetStoreItemsUseCase.java
@@ -1,6 +1,7 @@
 package pl.gamilife.gamification.application.usecase.getstoreitems.getall;
 
+import pl.gamilife.shared.kernel.architecture.Page;
 import pl.gamilife.shared.kernel.architecture.UseCase;
 
-public interface GetStoreItemsUseCase extends UseCase<GetStoreItemsCommand, GetStoreItemsResult> {
+public interface GetStoreItemsUseCase extends UseCase<GetStoreItemsCommand, Page<StoreItemDto>> {
 }
