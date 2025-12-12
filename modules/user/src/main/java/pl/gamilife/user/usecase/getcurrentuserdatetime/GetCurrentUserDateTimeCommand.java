5826f46177c5c3b3637a358410459ea7e75988e2
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-12-12 22:00:00 +0100

    feat: make use of new timezone annotation in task module

--- modules/user/src/main/java/pl/gamilife/user/usecase/getcurrentuserdatetime/GetCurrentUserDateTimeCommand.java
+++
@@ -1,9 +0,0 @@
-package pl.gamilife.user.usecase.getcurrentuserdatetime;
-
-import jakarta.validation.constraints.NotNull;
-import pl.gamilife.shared.kernel.architecture.Command;
-
-import java.util.UUID;
-
-public record GetCurrentUserDateTimeCommand(@NotNull UUID userId) implements Command {
-}
