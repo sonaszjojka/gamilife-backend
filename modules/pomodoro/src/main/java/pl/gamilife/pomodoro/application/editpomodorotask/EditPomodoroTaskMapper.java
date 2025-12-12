221eb6eb18a33f083894082fe249d91afb478c58
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-12-13 00:08:46 +0100

    refactor: refactor other pomodoro use case

--- modules/pomodoro/src/main/java/pl/gamilife/pomodoro/application/editpomodorotask/EditPomodoroTaskMapper.java
+++
@@ -1,8 +0,0 @@
-package pl.gamilife.pomodoro.application.editpomodorotask;
-
-import pl.gamilife.pomodoro.domain.model.PomodoroItem;
-
-
-public interface EditPomodoroTaskMapper {
-    EditPomodoroTaskResponse toResponse(PomodoroItem pomodoroItem);
-}
