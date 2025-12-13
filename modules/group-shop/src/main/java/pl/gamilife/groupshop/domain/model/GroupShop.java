c1c5c24933407ec207695a5ba5cea6ab1c0cbb15
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-13 21:23:17 +0100

    refactor: group shop model refactor

--- modules/group-shop/src/main/java/pl/gamilife/groupshop/domain/model/GroupShop.java
+++ modules/group-shop/src/main/java/pl/gamilife/groupshop/domain/model/GroupShop.java
@@ -1,48 +1,85 @@
 package pl.gamilife.groupshop.domain.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-
+import pl.gamilife.shared.kernel.exception.domain.DomainValidationException;
+import pl.gamilife.shared.persistence.entity.BaseEntity;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.UUID;
 
 @Getter
-@Setter
-@ToString
-@Builder
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "group_shop")
-public class GroupShop {
-    @Id
-    @Column(name = "group_shop_id", nullable = false)
-    private UUID groupShopId;
-
-    @Size(max = 100)
-    @NotNull
+@ToString
+public class GroupShop extends BaseEntity {
     @Column(name = "name", nullable = false, length = 100)
     private String name;
 
-    @Size(max = 500)
-    @NotNull
     @Column(name = "description", nullable = false, length = 500)
     private String description;
 
-    @NotNull
+
     @Column(name = "group_id", nullable = false)
     private UUID groupId;
 
-    @NotNull
+
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 
-    @Builder.Default
     @OneToMany(mappedBy = "groupShop")
     @ToString.Exclude
     private Set<GroupItemInShop> groupItemInShops = new LinkedHashSet<>();
+
+
+    private GroupShop(String name, String description, UUID groupId, Boolean isActive) {
+
+        setName(name);
+        setDescription(description);
+        setGroupId(groupId);
+        setIsActive(isActive);
+    }
+
+    public static GroupShop createForGroup(String name, String description, UUID groupId)
+    {
+        return new GroupShop(name, description, groupId, true);
+    }
+
+    public void setName(String name)
+    {
+        if (name == null||name.isBlank())
+        {
+            throw new DomainValidationException("Name cannot be null or blank");
+        }
+        this.name = name;
+    }
+
+    public void setGroupId(UUID groupId)
+    {
+        if (groupId == null)
+        {
+            throw new DomainValidationException("Group Id must be provided");
+        }
+    }
+
+    public void setDescription(String description)
+    {
+        if (description == null||description.isBlank())
+        {
+            throw new DomainValidationException("Description cannot be null or blank");
+        }
+
+    }
+
+    public void setIsActive(Boolean isActive)
+    {
+        if (isActive == null)
+        {
+            throw new DomainValidationException("Active status cannot be null");
+        }
+    }
+
+
 
 }
