0c79df65d98586d54ceded217a226c3f9d3ee2b4
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-14 01:32:19 +0100

    refactor: remove mappers

--- modules/group-shop/src/main/java/pl/gamilife/groupshop/application/editownedgroupitem/EditOwnedGroupItemMapper.java
+++
@@ -1,8 +0,0 @@
-package pl.gamilife.groupshop.application.editownedgroupitem;
-
-import pl.gamilife.groupshop.domain.model.OwnedGroupItem;
-
-public interface EditOwnedGroupItemMapper {
-
-    EditOwnedGroupItemResponse toResponse(OwnedGroupItem ownedGroupItem);
-}
