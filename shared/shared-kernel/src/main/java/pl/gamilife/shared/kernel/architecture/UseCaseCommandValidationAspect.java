93dda7ea4009ca67dc5ff08c3d5fc72681ab73de
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-12-27 16:51:12 +0100

    feat: add logging for processed commands

--- shared/shared-kernel/src/main/java/pl/gamilife/shared/kernel/architecture/UseCaseCommandValidationAspect.java
+++ shared/shared-kernel/src/main/java/pl/gamilife/shared/kernel/architecture/UseCaseCommandValidationAspect.java
@@ -2,8 +2,10 @@ package pl.gamilife.shared.kernel.architecture;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
 import jakarta.validation.Validator;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
+@Slf4j
 @Aspect
 @Order(1)
 @Component
@@ -22,13 +25,21 @@ public class UseCaseCommandValidationAspect {
 
     @Around("execution(* pl.gamilife..UseCase+.execute(..)) && args(command)")
     public Object autoValidate(ProceedingJoinPoint pjp, Command command) throws Throwable {
+        String commandName = command.getClass().getSimpleName();
+        log.info("Processing {}", commandName);
         Set<ConstraintViolation<Object>> violations = validator.validate(command);
 
         if (!violations.isEmpty()) {
+            log.error("Validation error(s) for {}. Violations: {}", commandName, violations);
             throw new ConstraintViolationException(violations);
         }
 
-        command.validate();
+        try {
+            command.validate();
+        } catch (Exception e) {
+            log.error("Complex validation error(s) for {}. Violations: {}", commandName, e.getMessage());
+            throw new ValidationException(e.getMessage());
+        }
 
         return pjp.proceed();
     }
