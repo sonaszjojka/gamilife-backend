5ceb5c065b8a2ca944bfbc42507c3c24d3cd3d1a
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2025-12-30 05:02:45 +0100

    feat: remove group shop with group

--- modules/group-shop/src/main/java/pl/gamilife/groupshop/domain/model/GroupShop.java
+++ modules/group-shop/src/main/java/pl/gamilife/groupshop/domain/model/GroupShop.java
@@ -1,9 +1,6 @@
 package pl.gamilife.groupshop.domain.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,7 +30,7 @@ public class GroupShop extends BaseEntity {
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 
-    @OneToMany(mappedBy = "groupShop")
+    @OneToMany(mappedBy = "groupShop", cascade = CascadeType.REMOVE)
     @ToString.Exclude
     private final Set<GroupItem> groupItems = new LinkedHashSet<>();
 
--- modules/group-shop/src/main/java/pl/gamilife/groupshop/infrastructure/event/handler/GroupShopEventHandler.java
+++ modules/group-shop/src/main/java/pl/gamilife/groupshop/infrastructure/event/handler/GroupShopEventHandler.java
@@ -5,20 +5,29 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import pl.gamilife.groupshop.application.creategroupshopforgroup.CreateGroupShopForGroupCommand;
 import pl.gamilife.groupshop.application.creategroupshopforgroup.CreateGroupShopForGroupUseCase;
+import pl.gamilife.groupshop.application.deletegroupshop.DeleteGroupShopCommand;
+import pl.gamilife.groupshop.application.deletegroupshop.DeleteGroupShopUseCase;
 import pl.gamilife.shared.kernel.event.GroupCreatedEvent;
+import pl.gamilife.shared.kernel.event.GroupDeletionRequestedEvent;
 
 @Component
 @AllArgsConstructor
 public class GroupShopEventHandler {
 
-    private final CreateGroupShopForGroupUseCase groupShopForGroupUseCase;
+    private final CreateGroupShopForGroupUseCase createGroupShopForGroupUseCase;
+    private final DeleteGroupShopUseCase deleteGroupShopUseCase;
 
     @EventListener
     public void handleGroupCreatedEvent(GroupCreatedEvent event) {
-        groupShopForGroupUseCase.execute(new CreateGroupShopForGroupCommand(
+        createGroupShopForGroupUseCase.execute(new CreateGroupShopForGroupCommand(
                 event.groupId(),
                 event.groupName()
         ));
+    }
+
+    @EventListener
+    public void handleGroupDeletionRequestedEvent(GroupDeletionRequestedEvent event) {
+        deleteGroupShopUseCase.execute(new DeleteGroupShopCommand(event.groupId()));
     }
 
 }
