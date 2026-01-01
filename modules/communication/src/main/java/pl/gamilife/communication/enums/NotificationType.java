9f486c99432d5bb33ddb0eabe7b6c3279c92085f
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2026-01-01 19:52:57 +0100

    feat: add notification types

--- modules/communication/src/main/java/pl/gamilife/communication/enums/NotificationType.java
+++ modules/communication/src/main/java/pl/gamilife/communication/enums/NotificationType.java
@@ -6,11 +6,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum NotificationType {
-    TASK_COMPLETED(1),
+    ACHIEVEMENT_UNLOCKED(1),
     ITEM_ACQUIRED(2),
     LEVEL_UP(3),
     GROUP_INVITATION(4),
     GROUP_ITEM_USED(5),
+    NEW_GROUP_MEMBER(6),
+    GROUP_MEMBER_LEFT(7),
+    NEW_GROUP_MESSAGE(8),
+    GROUP_TASK_ASSIGNED(9),
+    GROUP_TASK_COMPLETED(10),
+    GROUP_REQUEST_STATUS_UPDATED(11),
+    NEW_GROUP_REQUEST(12),
     OTHER(99);
 
     private final int id;
