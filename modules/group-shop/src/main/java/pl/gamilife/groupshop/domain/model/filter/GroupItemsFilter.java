9658d9b4836c05621508416f29266820c578d9bc
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2026-01-05 17:55:16 +0100

    refactor: use primitive types where possible

--- modules/group-shop/src/main/java/pl/gamilife/groupshop/domain/model/filter/GroupItemsFilter.java
+++ modules/group-shop/src/main/java/pl/gamilife/groupshop/domain/model/filter/GroupItemsFilter.java
@@ -1,17 +1,9 @@
 package pl.gamilife.groupshop.domain.model.filter;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.util.UUID;
 
 public record GroupItemsFilter(
-
-        @NotNull
         UUID groupShopId,
-
-        @NotNull
         Boolean isActive
 ) {
-
-
 }
